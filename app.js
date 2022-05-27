0ef1c6fcfafae6d7f6b1623c454c3aa73d4b1cca
a5748783 <a5748783@gmail.com>
2022-05-27 16:45:08 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const Restaurant = require('./models/restaurant')
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -29,10 +29,24 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 // routes setting
+// 瀏覽餐廳
 app.get('/', (req, res) => {
-  res.render('index', {
-    restaurants: restaurantList.results
-  })
+  Restaurant.find() // 取出 Restaurant model 裡的所有資料
+  .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+  .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
+  .catch(error => console.error(error))// 錯誤處理
+})
+
+// 新增餐廳頁面
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+// 新增餐廳
+app.post("/restaurants", (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
