9393494c2ed698a25481a8e886e38f36ea097772
a5748783 <a5748783@gmail.com>
2022-05-27 16:15:00 +0800

    feat: add handlebars

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Todo = require('../restaurant') // 載入 todo model
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once("open", () => {
+  console.log("running restaurantSeeder script...")
+
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log("restaurantSeeder done!")
+      db.close()
+    })
+    .catch(err => console.log(err))
+})
