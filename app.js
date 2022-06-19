bde19251aba3d312aff9e05b351206bbbc838c3f
a5748783 <a5748783@gmail.com>
2022-06-19 22:03:53 +0800

    feat: fill handlebars

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 // require packages used in the project
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const urlencoded = require('body-parser/lib/types/urlencoded')
 const methodOverride = require("method-override")
+const usePassport = require('./config/passport')
+const flash = require('connect-flash')
+const app = express()
+
 const routes = require('./routes')
 require('./config/mongoose')
 
-const app = express()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const port = 3000
 // setting template engine
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+usePassport(app)
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
 // setting static files
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
