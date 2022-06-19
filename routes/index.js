e73482b7632ebb2d75c0899eceead0c79672528e
a5748783 <a5748783@gmail.com>
2022-06-19 22:13:08 +0800

    feat: setting modules

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const router = express.Router()
 const home = require("./modules/home")
 const restaurants = require("./modules/restaurants")
 const users = require('./modules/users')
+const auth = require('./modules/auth')
+const { authenticator } = require('../middleware/auth')
 
-router.use("/", home)
-router.use("/restaurants", restaurants)
+router.use("/restaurants", authenticator, restaurants)
 router.use('/users', users)
+router.use('/auth', auth)
+router.use("/", home)
 
 module.exports = router
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,76 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login',
+  failureMessage: true
+}))
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email) {
+    errors.push({ message: '電子郵件欄位為必填' })
+  }
+  if (!password) {
+    errors.push({ message: '密碼欄位為必填' })
+  }
+  if (!confirmPassword) {
+    errors.push({ message: '確認密碼欄位為必填' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符' })
+  }
+
+  const errorMessage = {
+    errors,
+    name,
+    email,
+    password,
+    confirmPassword
+  }
+
+  if (errors.length) {
+    return res.render('register', errorMessage)
+  }
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        errors.push({ message: '這個 Email 已經註冊過了' })
+        return res.render('register', errorMessage)
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => {
+          User.create({
+            name,
+            email,
+            password: hash
+          })
+        })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '你已成功登出')
+  res.redirect('/users/login')
+})
+
 module.exports = router
